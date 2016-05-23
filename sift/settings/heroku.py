6274c2154ebc7f78401cfa2e2e1183e68a02dccb
Joe Nuff <industry.jhr@gmail.com>
2016-05-22 19:13:21 -0500

    --allow-empty-message

--- sift/settings/heroku.py
+++ sift/settings/heroku.py
@@ -9,9 +9,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = os.environ.get('DEBUG', False)
-DEBUG = False
+DEBUG = bool(os.environ.get('DEBUG', False))
 
-#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Database
