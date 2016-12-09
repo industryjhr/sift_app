35b0268e9762a711867ec6f227c94717d53825d2
Joe Nuff <industry.jhr@gmail.com>
2016-12-09 12:37:43 -0600

    More view tests

--- sift/concerts/test_views.py
+++ sift/concerts/test_views.py
@@ -17,30 +17,33 @@ class ViewsTest(TestCase):
 
         ## TODO move to fixture
         # seed DB
-        test_venue = Venue.objects.create(
+        self.test_venue = Venue.objects.create(
             name='House of Bugs', address='123 Card St., Chicago, IL 60606',
             schedule_url='http://bugs.rock'
         )
         # bypass Artist.add_artist
-        test_artist = Artist.objects.create(
+        self.test_artist = Artist.objects.create(
             name='Toast Test', spotify_id='123spotify',
             re_string='\b\bToast.Test\b\b', is_active=True
         )
-        test_concert = Concert.objects.create(pk=1,
-            billing='Toast Test w/ The Toe Jam', venue=test_venue,
+        self.test_concert = Concert.objects.create(pk=1,
+            billing='Toast Test w/ The Toe Jam', venue=self.test_venue,
             date_time=datetime.datetime.utcnow(), price='Free.99',
             url='http://bugs.rock/TTwTTJ'
         )
 
-        match = ConcertMatch.objects.create(concert=test_concert)
-        test_concert.artists.add(test_artist)
-        test_concert.save()
-        match.artists.add(test_artist)
-        match.save()
+        self.match = ConcertMatch.objects.create(concert=self.test_concert)
+        self.test_concert.artists.add(self.test_artist)
+        self.test_concert.save()
+        self.match.artists.add(self.test_artist)
+        self.match.save()
 
         # app URLs
-        self.home_url = '' #?
+        self.home_url = reverse('sift_home')
         self.upcoming_concerts_url = reverse('concerts:upcoming_shows')
+        self.artist_index_url = reverse('concerts:artists')
+        self.venue_index_url = reverse('concerts:venues')
+        self.concert_index_url = reverse('concerts:all_concerts')
 
     def test_home(self):
         """
@@ -57,9 +60,39 @@ class ViewsTest(TestCase):
         """
 
         response = self.client.get(self.upcoming_concerts_url)
-        self.assertEqual(len(response.context['matches']), 1)
+        match_data = response.context['matches']
+        self.assertEqual(len(match_data), 1)
         self.assertEqual(response.status_code, 200)
         self.assertGreater(
-            response.context['matches'][0].date_time,
+            match_data[0].date_time,
+            datetime.datetime(2010,1,1,tzinfo=datetime.timezone.utc)
+        )
+
+    def test_artists_index(self):
+        #pass
+        response = self.client.get(self.artist_index_url)
+        self.assertEqual(response.status_code, 200)
+        # accessing queryset response object from generic.ListView
+        artist_data = response.context[0]['object_list']
+        self.assertEqual(artist_data[0], self.test_artist)
+        self.assertEqual(len(artist_data), 1)
+
+    def test_venues_index(self):
+        response = self.client.get(self.venue_index_url)
+        self.assertEqual(response.status_code, 200)
+        # accessing queryset response object from generic.ListView
+        venue_data = response.context[0]['object_list']
+        self.assertEqual(venue_data[0], self.test_venue)
+        self.assertEqual(len(venue_data), 1)
+
+    def test_venues_index(self):
+        response = self.client.get(self.concert_index_url)
+        self.assertEqual(response.status_code, 200)
+        # accessing queryset response object from generic.ListView
+        concert_data = response.context['concert_list']
+        self.assertEqual(len(concert_data), 1)
+        self.assertEqual(concert_data[0], self.test_concert)
+        self.assertGreater(
+            response.context['last_updated'],
             datetime.datetime(2010,1,1,tzinfo=datetime.timezone.utc)
         )
