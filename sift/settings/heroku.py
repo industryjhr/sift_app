5ac929c94193b7ade08e81981fdbbc77a39d3167
Joe Ravesloot <industry.jhr@gmail.com>
2017-04-02 16:33:49 -0500

    Update a base

--- sift/settings/heroku.py
+++ sift/settings/heroku.py
@@ -30,11 +30,8 @@ MIDDLEWARE_CLASSES.append('rollbar.contrib.django.middleware.RollbarNotifierMidd
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-# DATABASES = {
-#     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-# }
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('HEROKU_POSTGRES_ONYX_URL'))
+    'default': dj_database_url.config(default=os.environ.get('HEROKU_POSTGRES_CYAN_URL'))
 }
 
 # Logging
