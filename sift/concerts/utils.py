fababde807dae11a20a9b7c9ae00e11365ca88fd
Joe Ravesloot <jravesloot@noblenetwork.org>
2017-08-05 09:58:53 -0500

    Add Spotify token

--- sift/concerts/utils.py
+++ sift/concerts/utils.py
@@ -1,7 +1,10 @@
 """
 concerts/utils.py
 
-Misc. utility functions for the concerts app.
+Misc. utility functions for the concerts app:
+
+* get_spotify_id
+* make_artist_regex
 """
 
 import logging, os
@@ -18,7 +21,7 @@ from .scrapers import (
     schubas,
 )
 
-LOGGER = logging.getLogger('concerts')
+logger = logging.getLogger('concerts')
 
 # key is Venue objects's pk; used by scrape_shows mgmt command
 SCRAPERS = {
@@ -34,10 +37,10 @@ SCRAPERS = {
 }
 
 FIXTURES_BASE_DIR = os.path.join(
-                        os.environ.get('SIFT_HOME'),
-                        'concerts',
-                        'fixtures'
-                    )
+    os.environ.get('SIFT_HOME'),
+    'concerts',
+    'fixtures'
+)
 
 FIXTURE_DIRS = {
     'artists': os.path.join(FIXTURES_BASE_DIR, 'artists'),
@@ -52,11 +55,18 @@ def get_spotify_id(artist_name):
     from django.utils.http import urlencode
 
     SPOTIFY_QUERY_BASE = 'https://api.spotify.com/v1/search?'
+    spotify_token = os.environ.get('SPOTIFY_TOKEN')
+    if spotify_token is None:
+        import sys
+        sys.exit("No SPOTIFY_TOKEN env var set. Sad!")
 
     # searching quoted artist name seems to give more accurate
     quoted_name = '\"' + artist_name + '\"'
     query_str = urlencode([('q', quoted_name), ('type', 'artist')])
-    response = requests.get(SPOTIFY_QUERY_BASE + query_str)
+    response = requests.get(
+        SPOTIFY_QUERY_BASE + query_str,
+        headers={"Authorization": "Bearer {}".format(spotify_token)}
+    )
     response.raise_for_status()
 
     artist_id = ''
@@ -64,9 +74,10 @@ def get_spotify_id(artist_name):
     try:
         artist_id = response.json()['artists']['items'][0]['id']
     except IndexError as e:
-        LOGGER.debug("Failed to get Spotify artist ID for {}".format(artist_name))
+        logger.debug("Failed to get Spotify artist ID for {}".format(artist_name))
 
     return artist_id
+
 
 def make_artist_regex(artist_name):
     """Returns regex string of artist name for storage in DB."""
@@ -83,6 +94,7 @@ def make_artist_regex(artist_name):
     no_end = re.sub(r'$', '\\\\b', no_punc)
     re_string = re.sub(r'^', '\\\\b', no_end)
     return re_string
+
 
 if __name__ == '__main__':
     pass
